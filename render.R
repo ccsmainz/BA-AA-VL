b7114e2a05101e5d319e695b5f3d3caa20e10e29
Michael Scharkow <michael@underused.org>
2024-04-18 09:44:55 +0200

    Fixes.

--- render.R
+++ render.R
@@ -7,8 +7,8 @@ NAME = "BA-AA-VL"
 list.files(".", ".qmd") |>
   walk(style_file)
 
-list.files(".", ".qmd") %>%
-  discard(str_detect, pattern = "literatur|index") %>%
+list.files(".", ".qmd") |>
+  discard(str_detect, pattern = "literatur.qmd|glossar.qmd|daten.qmd|index.qmd|ifpstats|SFConflict") |>
   walk(~ knitr::purl(
     input = .x, documentation = 2,
     output = str_c("code/", str_replace_all(.x, ".qmd", ".R"))
@@ -18,17 +18,19 @@ list.files(".", ".qmd") %>%
 prepend <- function(x) {
   txt1 <- "# VL Anwendungsorientierte Analyseverfahren, Institut für Publizistik, Uni Mainz"
   tstamp <- strftime(Sys.time(), "# %Y-%m-%d")
-  read_file(x) %>%
-    str_replace_all("#' ## ", "## ") %>%
-    str_remove_all("#'.*") %>%
-    str_remove_all("------.*") %>%
-    paste0(txt1, "\n", tstamp, "\n", .) %>%
-    str_replace_all("(\n){2,}", "\n\n") %>%
+  read_file(x) |>
+    # str_remove_all("#' ") |>
+    str_replace_all("#' ## ", "## ") |>
+    str_remove_all("#'.*") |>
+    str_remove_all("## --.*") |>
+    str_remove_all("## Glossar|## Hausaufgabe|## Folien") %>%
+    paste0(txt1, "\n", tstamp, "\n", .) |>
+    str_replace_all("(\n){2,}", "\n\n") |>
     write_file(file = x)
 }
 
 
-list.files("code", "*.R", full.names = T) %>%
+list.files("code", "*.R", full.names = T) |>
   walk(prepend)
 
 dir.create(NAME)
